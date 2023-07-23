1a05eaf705f619e336436c23b2ffc6056908596c
Say1ddd <sajidmuhammadikhlas@gmail.com>
2023-07-23 13:00:38 +0700

    Mengubah migration function

--- database/migrations/2024_06_14_021127_db_unprepared_kategori_function.php
+++ database/migrations/2024_06_14_021127_db_unprepared_kategori_function.php
@@ -12,26 +12,36 @@ return new class extends Migration
      */
     public function up(): void
     {
-        DB::unprepared('
-            CREATE FUNCTION `get_kategori`(`kategori` VARCHAR(4))
-            RETURNS VARCHAR(20)
-            DETERMINISTIC
-            BEGIN
-                DECLARE result VARCHAR(20);
-                IF kategori = "M" THEN
-                    SET result = "Modal";
-                ELSEIF kategori = "A" THEN
-                    SET result = "Alat";
-                ELSEIF kategori = "BHP" THEN
-                    SET result = "Barang Habis Pakai";
-                ELSEIF kategori = "BTHP" THEN
-                    SET result = "Barang Tidak Habis Pakai";
-                ELSE
-                    SET result = "Tidak Diketahui";
-                END IF;
-                RETURN result;
-            END'
-        );
+        // DB::unprepared('
+        //     CREATE FUNCTION `get_kategori`(`kategori` VARCHAR(4))
+        //     RETURNS VARCHAR(20)
+        //     DETERMINISTIC
+        //     BEGIN
+        //         DECLARE result VARCHAR(20);
+        //         IF kategori = "M" THEN
+        //             SET result = "Modal";
+        //         ELSEIF kategori = "A" THEN
+        //             SET result = "Alat";
+        //         ELSEIF kategori = "BHP" THEN
+        //             SET result = "Barang Habis Pakai";
+        //         ELSEIF kategori = "BTHP" THEN
+        //             SET result = "Barang Tidak Habis Pakai";
+        //         ELSE
+        //             SET result = "Tidak Diketahui";
+        //         END IF;
+        //         RETURN result;
+        //     END'
+        // );
+
+        // DB::unprepared('
+        //     CREATE FUNCTION `getKategoriName`(`id` INT)
+        //     RETURNS VARCHAR(255) CHARSET latin1
+        //     BEGIN
+        //         DECLARE kategori_name VARCHAR(255);
+        //         SELECT kategori INTO kategori_name FROM kategori WHERE id = id;
+        //         RETURN kategori_name;
+        //     END
+        // ');
     }
 
     /**
@@ -39,6 +49,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        DB::unprepared('DROP FUNCTION IF EXISTS `get_kategori`');
+        // DB::unprepared('DROP FUNCTION IF EXISTS `get_kategori`');
+        // DB::unprepared('DROP FUNCTION IF EXISTS `getKategoriName`');
     }
 };
