b11999c7c2f857322250ef19dc779fd4bd107fd6
Say1ddd <sajidmuhammadikhlas@gmail.com>
2023-07-29 13:36:13 +0700

    Mengubah model barangmasuk controller

--- app/Http/Controllers/BarangMasukController.php
+++ app/Http/Controllers/BarangMasukController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\BarangMasuk;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 use Inertia\Response;
+use App\Models\Barang;
+use App\Models\BarangMasuk;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
 
 class BarangMasukController extends Controller
 {
@@ -16,10 +18,9 @@ class BarangMasukController extends Controller
      */
     public function index(): Response
     {
-        $barangmasuks = BarangMasuk::with('barang')->get();
-
+        $barangmasuks = BarangMasuk::with('barang')->latest()->get();
         return Inertia::render('BarangMasuk/Index', [
-            'barangMasuk' => $barangmasuks,
+            'barangmasuks' => $barangmasuks,
         ]);
     }
 
@@ -28,12 +29,10 @@ class BarangMasukController extends Controller
      */
     public function create(): Response
     {
-        $barangmasuks = DB::table('barang_masuk')->get();
-        $barangs = DB::table('barang')->get();
+        $barangs = Barang::all();
 
         return Inertia::render('BarangMasuk/Create', [
-            'barangMasuk' => $barangmasuks,
-            'barang' => $barangs,
+            'barangs' => $barangs
         ]);
     }
 
@@ -46,91 +45,104 @@ class BarangMasukController extends Controller
         try {
             $request->validate([
                 'barang_id' => ['required', 'exists:barang,id'],
-                'jumlah' => ['required', 'numeric', 'min:1'],
-                'tanggal' => ['required', 'date'],
+                'qty_masuk' => ['required', 'numeric', 'min:1'],
+                'tgl_masuk' => ['required', 'date'],
             ]);
 
             BarangMasuk::create([
                 'barang_id' => $request->barang_id,
-                'jumlah' => $request->jumlah,
-                'tanggal' => $request->tanggal,
+                'qty_masuk' => $request->qty_masuk,
+                'tgl_masuk' => $request->tgl_masuk,
             ]);
 
             DB::commit();
             
-            return redirect()->route('barang-masuk.index')->with('success', 'Barang masuk berhasil ditambahkan');
+            return redirect()->route('barangmasuk.index')->with('success', 'Barang masuk berhasil ditambahkan');
         } catch (\Exception $e) {
             DB::rollBack();
-            return redirect()->route('barang-masuk.index')->with('error', 'Barang masuk gagal ditambahkan');
+            return redirect()->route('barangmasuk.index')->with('error', 'Barang masuk gagal ditambahkan' . $e->getMessage());
         }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(BarangMasuk $barangMasuk): Response
+    public function show($id): Response
     {
-        $barangmasuk = BarangMasuk::with('barang')->find($barangMasuk->id);
-
+        $barangMasuk = BarangMasuk::with('barang')->findOrFail($id);
+    
         return Inertia::render('BarangMasuk/Show', [
-            'barangMasuk' => $barangmasuk,
+            'barangmasuk' => $barangMasuk,
         ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(BarangMasuk $barangMasuk): Response
+    public function edit($id): Response
     {
-        $barangmasuk = BarangMasuk::with('barang')->find($barangMasuk->id);
+        $barangs = DB::table('barang')->get();
 
+        $barangMasuk = DB::table('barang_masuk')->find($id);
+    
         return Inertia::render('BarangMasuk/Edit', [
-            'barangMasuk' => $barangmasuk,
+            'barangmasuk' => $barangMasuk,
+            'barangs' => $barangs
         ]);
     }
+
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, BarangMasuk $barangMasuk): RedirectResponse
+    public function update(Request $request, $id): RedirectResponse
     {
         DB::beginTransaction();
         try {
+            $currentStok = Barang::findOrFail($request->barang_id)->stok - BarangMasuk::findOrFail($id)->qty_masuk;
+    
             $request->validate([
+                'tgl_masuk' => ['required', 'date'],
+                'qty_masuk' => ['required', 'numeric', 'min:' . $currentStok],
                 'barang_id' => ['required', 'exists:barang,id'],
-                'jumlah' => ['required', 'numeric', 'min:1'],
-                'tanggal' => ['required', 'date'],
+            ], [
+                'required' => ':attribute harus diisi',
+                'min' => ':attribute minimal :min',
             ]);
-
+    
+            $barangMasuk = BarangMasuk::findOrFail($id);
+    
             $barangMasuk->update([
+                'tgl_masuk' => $request->tgl_masuk,
+                'qty_masuk' => $request->qty_masuk,
                 'barang_id' => $request->barang_id,
-                'jumlah' => $request->jumlah,
-                'tanggal' => $request->tanggal,
             ]);
-
+    
             DB::commit();
-            
-            return redirect()->route('barang-masuk.index')->with('success', 'Barang masuk berhasil diubah');
+            return Redirect::route('barangmasuk.index')->with('success', 'Barang masuk berhasil diubah');
         } catch (\Exception $e) {
+            report($e);
             DB::rollBack();
-            return redirect()->route('barang-masuk.index')->with('error', 'Barang masuk gagal diubah');
+            return Redirect::back()->with('error', 'Barang masuk gagal diubah!' . $e->getMessage());
         }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(BarangMasuk $barangMasuk): RedirectResponse
+    public function destroy($id): RedirectResponse
     {
         DB::beginTransaction();
         try {
+            $barangMasuk = BarangMasuk::findOrFail($id);
             $barangMasuk->delete();
+    
             DB::commit();
-            
-            return redirect()->route('barang-masuk.index')->with('success', 'Barang masuk berhasil dihapus');
+            return Redirect::route('barangmasuk.index')->with('success', 'Barang masuk berhasil dihapus');
         } catch (\Exception $e) {
+            report($e);
             DB::rollBack();
-            return redirect()->route('barang-masuk.index')->with('error', 'Barang masuk gagal dihapus');
+            return Redirect::back()->with('error', 'Barang masuk gagal dihapus!');
         }
     }
 }
