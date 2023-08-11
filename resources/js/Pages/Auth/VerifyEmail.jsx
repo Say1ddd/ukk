6ea02636b675322e9f58e0360d8550beb51a18bf
Say1ddd <sajidmuhammadikhlas@gmail.com>
2023-08-11 14:17:36 +0700

    Mengubah nama import layout

--- resources/js/Pages/Auth/VerifyEmail.jsx
+++ resources/js/Pages/Auth/VerifyEmail.jsx
@@ -1,4 +1,4 @@
-import GuestLayout from '@/Layouts/GuestLayout';
+import AuthLayout from '@/Layouts/AuthLayout';
 import PrimaryButton from '@/Components/PrimaryButton';
 import { Head, Link, useForm } from '@inertiajs/react';
 
@@ -12,7 +12,7 @@ export default function VerifyEmail({ status }) {
     };
 
     return (
-        <GuestLayout>
+        <AuthLayout>
             <Head title="Email Verification" />
 
             <div className="mb-4 text-sm text-gray-600">
@@ -40,6 +40,6 @@ export default function VerifyEmail({ status }) {
                     </Link>
                 </div>
             </form>
-        </GuestLayout>
+        </AuthLayout>
     );
 }
