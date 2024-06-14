e88deebe34e170fa898f72dda45a7436f43503a1
Say1ddd <sajidmuhammadikhlas@gmail.com>
2024-06-14 14:28:31 +0700

    [invent-02] Tambah Kategori Baru

--- app/Http/Controllers/Api/KategoriController.php
+++ app/Http/Controllers/Api/KategoriController.php
@@ -21,9 +21,27 @@ class KategoriController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-        //
+        $request->validate([
+            'kategori' => ['required', 'max:255', 'unique:kategori,kategori'],
+            'deskripsi' => ['required', 'max:255']
+        ], [
+            'required' => ':attribute harus diisi',
+            'unique' => ':attribute sudah ada'
+        ]);
+    
+        $kategori = $request->kategori;
+    
+        $create = Kategori::create([
+            'deskripsi' => $request->deskripsi,
+            'kategori' => $kategori
+        ]);
+    
+        return response()->json([
+            'message' => 'Kategori berhasil dibuat',
+            'data' => $create
+        ], 201);
     }
 
     /**
